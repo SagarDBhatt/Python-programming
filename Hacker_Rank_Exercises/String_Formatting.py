0dfe8b9add8f4643dfb6b4f7679d68b26e410577
Sagarbhatt.ons1087@gmail.com <Ons@jhj@sa1087>
2021-02-11 11:14:38 -0700

    String formatting.

---
+++ Hacker_Rank_Exercises/String_Formatting.py
@@ -0,0 +1,55 @@
+# Problem Statement
+
+# Given an integer, , print the following values for each integer  from  to :
+#
+# Decimal
+# Octal
+# Hexadecimal (capitalized)
+# Binary
+# The four values must be printed on a single line in the order specified above for each  from  to . Each value should be space-padded to match the width of the binary value of .
+#
+# Input Format
+#
+# A single integer denoting .
+#
+# Constraints
+#
+# Output Format
+#
+# Print  lines where each line  (in the range ) contains the respective decimal, octal, capitalized hexadecimal, and binary values of . Each printed value must be formatted to the width of the binary value of .
+#
+# Sample Input
+#
+# 17
+# Sample Output
+#
+#     1     1     1     1
+#     2     2     2    10
+#     3     3     3    11
+#     4     4     4   100
+#     5     5     5   101
+#     6     6     6   110
+#     7     7     7   11117
+#     8    10     8  1000
+#     9    11     9  1001
+#    10    12     A  1010
+#    11    13     B  1011
+#    12    14     C  1100
+#    13    15     D  1101
+#    14    16     E  1110
+#    15    17     F  1111
+#    16    20    10 10000
+#    17    21    11 10001
+
+def print_formatted(number):
+    # print("Provided number = " + str(n))
+
+    for i in range(1, number + 1):
+        # print(i + " " + oct(i) + " " + hex(i).upper() + " " + bin(i))
+        width = len(bin(number)[2:])
+        print(str(i).rjust(width, ' '), str(oct(i)[2:]).rjust(width, ' '), str(hex(i).upper()[2:]).rjust(width, ' '),str(bin(i)[2:]).rjust(width, ' '))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
