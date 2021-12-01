d19a9f85071e47042523f5073ea15fcf1794c18e
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2021-12-01 11:05:38 -0700

    Exercise 01 Add numbers

---
+++ Python_Bootcamp/Chapter_2/Chapter 2 – Practice Set/01_Add_Numbers.py
@@ -0,0 +1,9 @@
+def AddNums(a, b):
+    print("Addition of given numbers = ", a + b)
+
+
+if __name__ == "__main__":
+    a = input("Enter first number = ")
+    a = int(a)
+    b = int(input("Enter seconf nummber = "))
+    AddNums(a, b)
