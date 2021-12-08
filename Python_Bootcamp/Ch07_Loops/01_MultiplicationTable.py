10571f2c176b3d08352fd690ae6a23f0545195e3
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2021-12-07 17:05:07 -0700

    Table with while loop

--- Python_Bootcamp/Ch07_Loops/01_MultiplicationTable.py
+++ Python_Bootcamp/Ch07_Loops/01_MultiplicationTable.py
@@ -2,6 +2,13 @@
 
 num = int(input("Enter table number = \n"))
 
-for i in range(1,11):
-    print(num, " * ",  i, " = ", num * i)
+for i in range(1, 11):
+    print(num, " * ", i, " = ", num * i)
 
+## Same problem with while loop.
+
+i = 1
+inp = int(input("Enter number = \n"))
+while i <= 10:
+    print(inp, " * ", i, " = ", inp * i)
+    i += 1
