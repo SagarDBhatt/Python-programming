b959bcfb2f94471a089bf8457b2e5dd31ddbbd94
sagarbhatt.ons1087@gmail.com <sagarbhatt.ons1087@gmail.com>
2021-12-10 12:25:11 -0700

    04 Star pattern with function

---
+++ Python_Bootcamp/08_Functions/05_StarPattern_Using_Function.py
@@ -0,0 +1,15 @@
+# ***
+#
+# **       #For n = 3
+#
+# *
+
+def pattern(n):
+    for i in range(1, n + 1):
+        for j in range(n, 0, -1):
+            if j >= i:
+                print("*", end="")
+        print()
+
+
+pattern(3)
